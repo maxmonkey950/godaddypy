389fbf8149b0f878bc7c05364c94d95f244ab678
maxmonkey950 <46515596+maxmonkey950@users.noreply.github.com>
2019-10-14 14:36:08 +0800

    Update gody.py

--- gody.py
+++ gody.py
@@ -9,11 +9,11 @@ client = Client(my_acct)
 delegate_client = Client(delegate_acct)
 
 def adddns(asd):
-    t = client.get_domain_info(asd)
-    p = ['ns23.domaincontrol.com', 'ns24.domaincontrol.com']
-    if t.get('nameServers') != p:
-        client.update_domain(asd, nameServers = ['ns23.domaincontrol.com', 'ns24.domaincontrol.com'])
-        time.sleep(20)
+    #t = client.get_domain_info(asd)
+    #p = ['ns23.domaincontrol.com', 'ns24.domaincontrol.com']
+    #if t.get('nameServers') != p:
+    #    client.update_domain(asd, nameServers = ['ns23.domaincontrol.com', 'ns24.domaincontrol.com'])
+    #    time.sleep(20)
     try:
         client.delete_records(asd, name='www', record_type='CNAME')
         time.sleep(3)
@@ -32,7 +32,6 @@ def adddns(asd):
         time.sleep(5)
         client.add_record(asd, {'data':'v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrLHiExVd55zd/IQ/J/mRwSRMAocV/hMB3jXwaHH36d9NaVynQFYV8NaWi69c1veUtRzGt7yAioXqLj7Z4TeEUoOLgrKsn8YnckGs9i3B3tVFB+Ch/4mPhXWiNfNdynHWBcPcbJ8kjEQ2U8y78dHZj1YeRXXVvWob2OaKynO8/lQIDAQAB;','name':'mandrill._domainkey','ttl':600, 'type':'TXT'})
     else:
-    #client.delete_records(asd, name='www', record_type='CNAME')
         client.add_record(asd, {'data':'shops.myshopify.com','name':'www','ttl':600, 'type':'CNAME'})
         time.sleep(3)
         client.add_record(asd, {'data':'23.227.38.32','name':'letter','ttl':600, 'type':'A'})

91c9f7ea1762c6f18a70ec9347b275212dc2f3fe
maxmonkey950 <maxmonkey950@gmail.com>
2019-11-13 08:33:03 +0000

    update checkdomain.py

--- checkdomain.py
+++ checkdomain.py
@@ -19,8 +19,9 @@ def get_domains(marker=""):
         params.append(('marker', marker))
     response = requests.get('https://api.godaddy.com/v1/domains', headers=headers, params=params)
     res_list = response.json()
+    #total_datas.extend(res_list)
     for r in res_list:
-        print(r)
+        #print(r)
         total_datas[r["domain"]] = (r["createdAt"], r["expires"], r["nameServers"], r["renewAuto"], r["status"])
     # for v in res_list:
     #     print(v)
@@ -30,31 +31,42 @@ def get_domains(marker=""):
         get_domains(marker=m)
 
 def is_need_update(domain, status):
-    if total_datas.get(domain, "") != status:
+    if total_datas.get(domain, "")[4] != status:
         return True
     else:
         return False
 
 def update_db():
     global total_datas
+    #old_data = cur.execute("select id, domain, status from dns where id>0")
     cur.execute("select domains, dnsstatus from public.dns")
     old_data = cur.fetchall()
     need_update_data = []
     for d in old_data:
-        if is_need_update(d[0], d[1]):
-            print(total_datas)
-            need_update_data.append((total_datas.get(d[0].strip(), [])[4], d[0]))
-
+        #print("-------------", d[0])
+        #print(d)
+        #print(type(d))
+        try:
+            if is_need_update(d[0], d[1]):
+            #need_update_data.append((d["id"], status))
+            #print(total_datas)
+            #need_update_data.append((total_datas.get(d[0].strip(), [])[0], d[0]))
+                need_update_data.append((total_datas.get(d[0].strip(), [])[4], d[0]))
+        except Exception as e:
+            print(e)
     print(need_update_data)
     for ts in need_update_data:
-        print(ts)
+        #print(ts)
         cur.execute("update public.dns set dnsstatus = '%s' where domains = '%s'" %(ts[0], ts[1].strip()))
         conn.commit()
 
 def init_data():
+    #TRUNCATE TABLE
     global total_datas
     for k, v in total_datas.items():
         #print(k, v)
+        # print(d['domain'], d['createdAt'], d['renewAuto'], d['status'])
+        #k = k.strip()
         cur.execute("INSERT INTO public.dns VALUES (5067421, 'dns', 'None', 'None', 'A', 'admin', '2019-10-30 13:38:57.464686', 'NULL', %s,%s,%s,%s,%s,%s)",(k, v[0],v[1],v[2],v[3],v[4]))
         conn.commit()
 if __name__ == '__main__':
